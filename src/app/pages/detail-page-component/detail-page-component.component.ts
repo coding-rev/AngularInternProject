868192bccc7f7619dfa81faff21facb487a6c648
Manuel <emmanuel.owusu@turntabl.io>
2022-06-28 16:04:09 +0000

    feat: initialized detail page component

---
+++ src/app/pages/detail-page-component/detail-page-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-page-component',
+  templateUrl: './detail-page-component.component.html',
+  styleUrls: ['./detail-page-component.component.css']
+})
+export class DetailPageComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
