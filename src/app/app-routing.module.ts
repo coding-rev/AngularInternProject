5d9872b79cb4f87fc8f145c04236ae4e42695133
Aceofthehood1 <geoffreyameble@gmail.com>
2022-06-30 00:30:34 +0100

    Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonDetailsComponent } from './components/pokemon-details/pokemon-details.component';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
+import { pokemonData } from './mock/data';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'pokemon', component: PokemonComponent},
+  {path: 'pokemon-details', component: PokemonDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+//Routing 
+export const routingComponents = [PokemonComponent, PokemonDetailsComponent];
